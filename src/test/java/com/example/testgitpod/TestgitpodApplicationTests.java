a961793856757acfe09dc99280049521377a579d
huangjie <huangjie@regentsoft.net>
2022-05-12 10:29:58 +0800

    Initial commit

---
+++ src/test/java/com/example/testgitpod/TestgitpodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testgitpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitpodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
